7b4cd46672ef003b621b0afd2e10fa7196aa06bb
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-06-04 15:30:47 +0530

    RESPONSIVE and footer

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+
+
+function navHIDE()
+{
+ var x=document.getElementById("smallerVP");
+ 
+ if(x.style.display=="block")
+ {
+     x.style.display="none";
+    
+
+ }
+ else{
+     x.style.display="block";
+     x.style.float="right";
+ }
+}
