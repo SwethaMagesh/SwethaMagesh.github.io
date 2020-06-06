e6d0ca03df0292d54090809ea4828f0f6e356fe3
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-06-06 12:32:53 +0530

    Some js for opening and closing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,43 @@
-// This function positions the footer based on window size
+function displayproCard(){
+    var x=document.getElementsByClassName("pro");
+    if(x.style.display=="block")
+ {
+     x.style.display="none";
+    
+
+ }
+ else{
+     x.style.display="block";
+     
+ }
+
+}
+
+function historyIn(){
+
+    var x=document.getElementById("history");
+    if(x.style.display=="none")
+    x.style.display="block";
+    else
+    x.style.display="none";
+
+
+     
+}
+
+function codes(){
+
+    var x=document.getElementById("codes");
+    if(x.style.display=="none")
+    x.style.display="block";
+    else
+    x.style.display="none";
+
+
+     
+}
+
+// This function based on window size
 function hideMobBar(){
     var $containerw = $(window).width();
     if ($containerw >= 818) {
