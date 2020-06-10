27452d0b1dbe7900a906b840c0640cccd2f74450
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-06-10 21:57:57 +0530

    Nav bar for responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 
-
+function NavTog(){
+    var x=document.querySelector(".nav-icon");
+    x.classList.toggle('toggle');
+    var y=document.querySelector(".topbar");
+    y.classList.toggle('top-bar-active');
+}
 function historyIn(){
 
     var x=document.getElementById("history");
