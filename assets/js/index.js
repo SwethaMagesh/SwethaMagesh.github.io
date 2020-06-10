87d777ebbb47f63ab51f5e2e9c54786c6f0f0801
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-06-10 09:00:43 +0530

    Sans nav bar and back to top arrow

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,4 @@
-function displayproCard(){
-    var x=document.getElementsByClassName("pro");
-    if(x.style.display=="block")
- {
-     x.style.display="none";
-    
 
- }
- else{
-     x.style.display="block";
-     
- }
-
-}
 
 function historyIn(){
 
@@ -37,37 +24,3 @@ function codes(){
      
 }
 
-// This function based on window size
-function hideMobBar(){
-    var $containerw = $(window).width();
-    if ($containerw >= 818) {
-        $('#smallerVP').css({
-           display:'none'
-        });
-    }
-    
-}
-
-$(document).ready(function () {
-    hideMobBar();//run when page first loads
-});
-
-$(window).resize(function () {
-    hideMobBar();//run on every window resize
-});
-
-function navHIDE()
-{
- var x=document.getElementById("smallerVP");
- 
- if(x.style.display=="block")
- {
-     x.style.display="none";
-    
-
- }
- else{
-     x.style.display="block";
-     x.style.float="right";
- }
-}
