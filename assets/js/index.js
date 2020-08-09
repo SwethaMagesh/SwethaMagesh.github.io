0787232ed491dc8e288866102f47b6210c13aeda
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-08-09 23:15:30 +0530

    Some changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,27 +9,7 @@ function NavTog(){
 function ProToggle(){
 
 }
-function historyIn(){
-
-    var x=document.getElementById("history");
-    if(x.style.display=="block")
-    x.style.display="none";
-    else
-    x.style.display="block";
 
 
-     
-}
 
-function codes(){
-
-    var x=document.getElementById("codes");
-    if(x.style.display=="block")
-    x.style.display="none";
-    else
-    x.style.display="block";
-
-
-     
-}
 
